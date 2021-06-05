c172e10a5c9e2de5eff473f3dd8ea7e1f40de60d
AlekzV <alekzgrinder@gmail.com>
2021-06-05 16:29:28 -0500

    Added meals components

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
   {
@@ -30,11 +32,18 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li key={meal.id}>{meal.name}</li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
---
+++ src/components/UI/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+const Input = (props) => {
+  const { input, label } = props;
+
+  return (
+    <div className={classes.input}>
+      <label htmlFor={input.id}>{label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;

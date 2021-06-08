1c424105b2df813e645f90bb52051b967f6c897e
AlekzV <alekzgrinder@gmail.com>
2021-06-07 21:52:40 -0500

    Added cart code

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Cart.module.css';
+
+const Cart = () => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Chocotorta', price: 12.99, amount: 2 }].map(
+        (item) => (
+          <li>{item.name}</li>
+        )
+      )}
+    </ul>
+  );
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>25.88</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;

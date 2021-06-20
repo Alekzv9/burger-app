1ec0b223fcd394cab2f9d5576f2982e00a9fd4d5
AlekzV <alekzgrinder@gmail.com>
2021-06-20 15:39:07 -0500

    Adding cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount =
+        state.totalAmount + (action.item.amount + action.item.price);
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    case 'REMOVE':
+      return '';
+    default:
+      return defaultCartState;
+  }
+};
+
 export const CartProvider = (props) => {
+  // Maybe change this
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  const addItemToCartHandler = (item) => {};
+  const [cartState, dispatch] = useReducer(cartReducer, defaultCartState);
 
-  const removeItemFromCartHandler = (id) => {};
+  const addItemToCartHandler = (item) => {
+    dispatch({ type: 'ADD', item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatch({ type: 'REMOVE', id });
+  };
 
   const cartShownHandler = () => {
     console.log('heree');
@@ -17,8 +46,8 @@ export const CartProvider = (props) => {
 
   const cartContext = {
     cartIsShown,
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     onShowCart: cartShownHandler,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,

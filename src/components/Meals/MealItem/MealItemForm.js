7fe1fceca04b878931b484ed6da4624f7194a862
AlekzV <alekzgrinder@gmail.com>
2021-06-20 15:47:51 -0500

    Working with Refs & Forward Refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const inputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = +inputRef.current.value;
+    if (!enteredAmount || enteredAmount < 1 || enteredAmount > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmount);
+  };
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <Input
+        ref={inputRef}
         label='Amount'
         input={{
           id: 'amount_' + props.id,
@@ -17,6 +33,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount 1 - 5</p>}
     </form>
   );
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   const { input, label } = props;
 
   return (
     <div className={classes.input}>
       <label htmlFor={input.id}>{label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;

54f3736e36cfa215ed5db80103c1029c12fa2ee6
AlekzV <alekzgrinder@gmail.com>
2021-06-20 16:51:06 -0500

    Using the useEffect Hook

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,22 +1,42 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const { items } = useContext(CartContext);
+  const [btnIsHightlighted, setBtnIsHightlighted] = useState(false);
 
   const numberOfItems = items.reduce(
     (accumulator, current) => accumulator + current.amount,
     0
   );
 
+  const btnClasses = `${classes.button} ${
+    btnIsHightlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (!items.length) {
+      return;
+    }
+    setBtnIsHightlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHightlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   const onClickHandler = () => {
     props.onClick(true);
   };
 
   return (
-    <button onClick={onClickHandler} className={classes.button}>
+    <button onClick={onClickHandler} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
